f61b89ab62c3122ed0256347309850ea469541a8
Basil ALBADER <baalbade@student.42.fr>
2023-01-04 14:25:43 +0100

    libft

---
+++ libft/ft_isprint.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 13:21:34 by baalbade          #+#    #+#             */
+/*   Updated: 2022/12/13 10:46:31 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isprint(int c)
+{
+	return ((unsigned char)c >= FIRST_PRINTABLE
+		&& (unsigned char)c <= LAST_PRINTABLE);
+}

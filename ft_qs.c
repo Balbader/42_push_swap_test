2ec822d1d0b0abee2998778a088c61ec90720e6f
Basil ALBADER <baalbade@student.42.fr>
2023-01-09 13:12:36 +0100

    ft_qs.c

--- ft_qs.c
+++ ft_qs.c
@@ -5,16 +5,51 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/04 12:39:05 by baalbade          #+#    #+#             */
-/*   Updated: 2023/01/04 12:39:08 by baalbade         ###   ########.fr       */
+/*   Created: 2023/01/09 13:11:17 by baalbade          #+#    #+#             */
+/*   Updated: 2023/01/09 13:11:22 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-#include <stdio.h>
-
-int	main(void)
+void	ft_swap(int *a, int *b)
 {
-	return (0);
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int	ft_partition(int *arr, int lo, int hi)
+{
+	int	pivot;
+	int	idx;
+	int	i;
+
+	pivot = arr[hi];
+	idx = lo - 1;
+	i = lo;
+	while (i < hi)
+	{
+		if (arr[i] <= pivot)
+		{
+			idx++;
+			ft_swap(&arr[i], &arr[idx]);
+		}
+		++i;
+	}
+	idx++;
+	arr[hi] = arr[idx];
+	arr[idx] = pivot;
+	return (idx);
+}
+
+void	ft_quicksort(int *arr, int lo, int hi)
+{
+	int	pivot_idx;
+
+	if (lo >= hi)
+		return ;
+	pivot_idx = ft_partition(arr, lo, hi);
+	ft_quicksort(arr, lo, pivot_idx - 1);
+	ft_quicksort(arr, pivot_idx + 1, hi);
 }

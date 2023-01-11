6c4c5e0dae9278c7adb475ce1949cf818bb6d3e3
Basil ALBADER <baalbade@student.42.fr>
2023-01-11 10:02:02 +0100

    isalpha

--- libft/srcs/ctype/ft_isalpha.c
+++ libft/srcs/ctype/ft_isalpha.c
@@ -14,5 +14,7 @@
 
 int	ft_isalpha(int c)
 {
-	return (ft_islower((unsigned char)c) || ft_isupper((unsigned char)c));
+	if (!(c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	return (0);
 }

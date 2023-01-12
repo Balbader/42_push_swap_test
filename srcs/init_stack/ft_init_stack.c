2f2af0d04789b3a4106195e60720752228622c07
Basil ALBADER <baalbade@student.42.fr>
2023-01-12 14:26:43 +0100

    ft_init_stack.c

--- srcs/init_stack/ft_init_stack.c
+++ srcs/init_stack/ft_init_stack.c
@@ -15,29 +15,7 @@
 	stack (be careful about the order).
 */
 
-// #include "push_swap.h"
-#include <stdlib.h>
-#include "ft_convert_entry.c"
-
-typedef struct s_dlist
-{
-	void			*data;
-	struct s_dlist	*prev;
-	struct s_dlist	*next;
-}				t_dlist;
-
-t_dlist	*ft_dlist_create(void *data)
-{
-	t_dlist	*new;
-
-	new = (t_dlist *)calloc(1, sizeof(t_dlist));
-	if (!new)
-		return (NULL);
-	new->data = data;
-	new->prev = NULL;
-	new->next = NULL;
-	return (new);
-}
+#include "../../includes/push_swap.h"
 
 static int	ft_arr_size(int *arr)
 {
@@ -54,21 +32,7 @@ t_dlist	*ft_init_stack(int *arr)
 	t_dlist	*stack_a;
 	t_dlist	*head_a;
 	int		arr_size;
-	int		*ptr;
 	int		i;
 
-	ptr = malloc(sizeof(int));
-	if (!ptr)
-		return (NULL);
-	arr_size = ft_arr_size(arr);
-	i = 0;
-	ptr = &i;
-	ft_dlist_create(ptr);
-	++i;
-	while (i < arr_size)
-	{
-		ptr = &i;
-		++i;
-	}
 	return (head_a);
 }

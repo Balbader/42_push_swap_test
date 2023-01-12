1b302a5d76cdc8fb9a275e5561d280169fe59b90
Basil ALBADER <baalbade@student.42.fr>
2023-01-12 07:33:07 +0100

    dlist test

--- test/main.c
+++ test/main.c
@@ -46,6 +46,8 @@ t_dlist	*ft_init_stack_a(int *arr, int arr_size)
 	t_dlist	*stack_a;
 	int		i;
 
+	if ((!arr) || (!arr_size))
+		return (NULL);
 	i = 0;
 	new_node = ft_create_node(arr[i]);
 	stack_a = new_node;
@@ -60,9 +62,48 @@ t_dlist	*ft_init_stack_a(int *arr, int arr_size)
 	return (stack_a);
 }
 
+t_dlist	*ft_find_node(t_dlist *dlist, int data)
+{
+	t_dlist	*to_find;
+
+	if (!dlist)
+		return (NULL);
+	to_find = dlist;
+	while (to_find != NULL)
+	{
+		if (to_find->data == data)
+			return (to_find);
+		to_find = to_find->next;
+	}
+	return (to_find);
+}
+
+t_dlist	*ft_insert_node(t_dlist *dlist, int data, t_dlist *node_to_insert)
+{
+	t_dlist	*new_dlist;
+	t_dlist	*current;
+	t_dlist	*to_find;
+
+	to_find = ft_find_node(dlist, data);
+	current = dlist;
+	new_dlist = current;
+	while (current != NULL)
+	{
+		if (current == to_find)
+		{
+			current->next = node_to_insert;
+			node_to_insert->prev = current;
+			node_to_insert->next = current->next;
+			current->next->prev = node_to_insert;
+		}
+	}
+	return (new_dlist);
+}
+
 int	main(void)
 {
 	t_dlist *stack_a;
+	t_dlist	*to_find;
 	int		*arr = NULL;
 	int		arr_size;
 	int		i;
@@ -70,6 +111,8 @@ int	main(void)
 	arr_size = 10;
 	arr = ft_init_arr(arr, arr_size);
 	stack_a = ft_init_stack_a(arr, arr_size);
+	to_find = ft_find_node(stack_a, 5);
+	printf("to_find->data: %d\n", to_find->data);
 	i = 1;
 	while (stack_a != NULL)
 	{

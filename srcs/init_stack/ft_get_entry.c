23bf22e75395760e59d8b1ce6b599d599b1d3f9a
Basil ALBADER <baalbade@student.42.fr>
2023-01-12 14:28:40 +0100

    ft_get_entry.c

---
+++ srcs/init_stack/ft_get_entry.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_entry.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 14:27:55 by baalbade          #+#    #+#             */
+/*   Updated: 2023/01/12 14:28:05 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+int	*ft_get_entry(int ac, char **av)
+{
+	int	*arr;
+	int	j;
+	int	i;
+
+	arr = (int *)malloc(sizeof(int) * (ac));
+	if (!arr)
+		return (0);
+	j = 1;
+	i = 0;
+	while (j < ac)
+	{
+		arr[i] = ft_atoi(av[j]);
+		++j;
+		++i;
+	}
+	arr[i] = 0;
+	return (arr);
+}

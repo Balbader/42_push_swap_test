db1c46a7c82f2b7940f0250b3f100394e798490e
Basil ALBADER <baalbade@student.42.fr>
2023-01-13 14:11:07 +0100

    main test file

--- srcs/operations/test/dlist/main.c
+++ srcs/operations/test/dlist/main.c
@@ -2,7 +2,9 @@
 
 int	main(void)
 {
-	t_dlist	*stack_a;
+	t_dlist	*stack;
+	t_dlist	*new;
+	t_dlist	*new_stack;
 	int		*arr;
 	int		arr_size;
 
@@ -10,8 +12,19 @@ int	main(void)
 	arr = (int *)malloc(sizeof(int) * arr_size);
 	if (!arr)
 		return (0);
+	
+	// Test for function init stack
 	arr = ft_init_arr(arr, 10);
-	stack_a = ft_init_stack(arr, arr_size);
-	ft_dlist_print(stack_a);
-	return (0);
+	stack = ft_init_stack(arr, arr_size);
+	printf("STACK :\n");
+	ft_dlist_print(stack);
+
+	printf("\n");
+	
+	// Test for function insert new node in stack
+	new = ft_dlist_create(42);
+	new_stack = ft_dlist_insert_node(stack, 5, 42);
+	printf("NEW STACK :\n");
+	ft_dlist_print(new_stack);
+return (0);
 }

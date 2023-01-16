73dd7976d07b8f61bdda9cd98787600b42dce353
Basil ALBADER <baalbade@student.42.fr>
2023-01-16 14:00:43 +0100

    New version

---
+++ PUSH_SWAP/srcs/1_eheck_entry/ft_check_entry.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_entry.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 11:09:05 by baalbade          #+#    #+#             */
+/*   Updated: 2023/01/16 11:09:13 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_push_swap.h"
+
+static int	ft_isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int	ft_check_entry(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '-')
+		++i;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (1);
+		++i;
+	}
+	return (0);
+}

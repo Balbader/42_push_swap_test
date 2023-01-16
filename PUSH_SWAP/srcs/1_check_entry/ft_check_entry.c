c8756fa7f66b33a9ef75bf50169cea984a33e924
Basil ALBADER <baalbade@student.42.fr>
2023-01-16 17:27:59 +0100

    ft_check_entry.c

--- PUSH_SWAP/srcs/1_check_entry/ft_check_entry.c
+++ PUSH_SWAP/srcs/1_check_entry/ft_check_entry.c
@@ -10,7 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_push_swap.h"
+#include "../../includes/push_swap.h"
+
+static int	ft_isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
 
 int	ft_check_entry(char *str)
 {

3b6fa07e61597270af487d15e74ab76ac3544349
Basil ALBADER <baalbade@student.42.fr>
2023-01-16 17:03:07 +0100

    push_swap.h

---
+++ PUSH_SWAP/includes/push_swap.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 15:43:17 by baalbade          #+#    #+#             */
+/*   Updated: 2023/01/16 15:43:22 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/includes/libft.h"
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+#include <stdio.h>
+
+// **************** check errors *****************
+int			ft_check_entry(char *str);
+int			ft_convert_entry(char *str);
+
+#endif

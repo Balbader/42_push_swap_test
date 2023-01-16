53289823eab65765957203585ff814fe9c58e9d6
Basil ALBADER <baalbade@student.42.fr>
2023-01-16 17:28:58 +0100

    ft_convert_entry.c

--- PUSH_SWAP/srcs/2_convert_entry/ft_convert_entry.c
+++ PUSH_SWAP/srcs/2_convert_entry/ft_convert_entry.c
@@ -10,7 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_push_swap.h"
+#include "../../includes/push_swap.h"
+
+static int	ft_isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+static int	ft_atoi(char *str)
+{
+	int	res;
+	int	sign;
+	int	i;
+
+	res = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ')
+		++i;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		++i;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		res = res * 10 + str[i] - '0';
+		++i;
+	}
+	return (res * sign);
+}
 
 int	ft_convert_entry(char *str)
 {

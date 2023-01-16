125feaa147eb73b92abcddf20e9e8d0247d659cf
Basil ALBADER <baalbade@student.42.fr>
2023-01-16 14:26:34 +0100

    .h

--- PUSH_SWAP/includes/ft_push_swap.h
+++ PUSH_SWAP/includes/ft_push_swap.h
@@ -19,6 +19,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
+#include <stdio.h>
 
 typedef struct s_dlist
 {
@@ -27,40 +28,8 @@ typedef struct s_dlist
 	struct s_dlist	*prev;
 }				t_dlist;
 
-// **************** quicksort ********************
-static int	ft_partition(int *arr, int lo, int hi);
-static void	ft_swap(int *a, int *b);
-void		ft_qs(int *arr, int lo, int hi);
-
-// **************** init stack *******************
-int			*ft_get_entry(int ac, char **av);
-static int	ft_arr_size(int *arr);
-t_dlist		*ft_init_stack_a(int *arr, int arr_size);
-
 // **************** check errors *****************
-static int	ft_is_minus(int c);
-static int	ft_check_str(char *str);
-static void	ft_check_duplicates(int *arr);
-void		ft_check_main_arg(int ac, char **av);
-void		ft_check_errors(int *arr);
-
-// ***************** dbl_list *******************
-t_dlist		*ft_create_node(int data);
-t_dlist		*ft_find_node(t_dlist *dlist, int data);
-t_dlist		*ft_init_stack_a(int *arr, int arr_size);
-void		ft_insert_node(t_dlist *dlist, int data, t_dlist *to_find);
-
-// **************** operations ******************
-void		ft_swap_a(t_dlist *list);
-void		ft_swap_b(t_dlist *list);
-void		ft_ss(t_dlist *list);
-void		ft_push_a(t_dlist *list);
-void		ft_push_b(t_dlist *list);
-void		ft_rotate_a(t_dlist *list);
-void		ft_rotate_b(t_dlist *list);
-void		ft_rr(t_dlist *list);
-void		ft_reverse_rotate_a(t_dlist *list);
-void		ft_reverse_rotate_b(t_dlist *list);
-void		ft_rrr(t_dlist *list);
+int			ft_check_entry(char *str);
+int			ft_convert_entry(char *str);
 
 #endif

911ba9be10a9b87b18af0e0ab16bd17986fb79b6
Basil ALBADER <baalbade@student.42.fr>
2023-01-18 10:08:26 +0100

    update convert entry function

--- PUSH_SWAP/src/2_convert_entry/ft_convert_entry.c
+++ PUSH_SWAP/src/2_convert_entry/ft_convert_entry.c
@@ -12,36 +12,6 @@
 
 #include "push_swap.h"
 
-static int	ft_isdigit(char c)
-{
-	return (c >= '0' && c <= '9');
-}
-
-static int	ft_atoi(char *str)
-{
-	int	res;
-	int	sign;
-	int	i;
-
-	res = 0;
-	sign = 1;
-	i = 0;
-	while (str[i] == ' ')
-		++i;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign *= -1;
-		++i;
-	}
-	while (ft_isdigit(str[i]))
-	{
-		res = res * 10 + str[i] - '0';
-		++i;
-	}
-	return (res * sign);
-}
-
 int	ft_convert_entry(char *str)
 {
 	return (ft_atoi(str));

6190a679bcc3fe4aa2a014f6ad4a41873a935ec0
Basil ALBADER <baalbade@student.42.fr>
2023-01-21 09:10:22 +0100

    all tests

--- PS_TEST_2/test/test_runners/all_tests.c
+++ PS_TEST_2/test/test_runners/all_tests.c
@@ -18,7 +18,7 @@ static void	run_all_tests(void)
 	RUN_TEST_GROUP(Entries);
 }
 
-int	main(int ac, char *av[])
+int	main(int argc, char *argv[])
 {
 	return (UnityMain(argc, argv, run_all_tests));
 }

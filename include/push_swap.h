46d0eee09b462d29f26340d4d289cc61303f933c
Basil ALBADER <baalbade@student.42.fr>
2023-01-23 14:37:33 +0100

    .h files

---
+++ include/push_swap.h
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 15:43:17 by baalbade          #+#    #+#             */
+/*   Updated: 2023/01/16 15:43:22 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/include/libft.h"
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <stdio.h>
+
+typedef struct s_dblist
+{
+	int				data;
+	struct s_dblist	*next;
+	struct s_dblist	*prev;
+}				t_dblist;
+
+// **************** check errors *****************
+int			ft_check_entry(char *str);
+// int			test_ft_check_entry(char *str);
+
+int			ft_convert_entry(char *str);
+// int			test_ft_convert_entry(char *str);
+
+t_dblist	*ft_store_entry(int data);
+// t_dblist	*test_ft_store_entry(int data);
+
+// ***************** dbl_list *******************
+t_dblist	*ft_create_node(int data);
+// t_dblist	*test_ft_create_node(int data);
+
+t_dblist	*ft_find_node(t_dblist *dlist, int data);
+// t_dblist	*test_ft_find_node(t_dblist *dlist, int data);
+
+t_dblist	*ft_init_stack_a(int *arr, int arr_size);
+// t_dblist	*test_ft_init_stack_a(int *arr, int arr_size);
+
+void		ft_insert_node(t_dblist *dlist, int data, t_dblist *to_find);
+// void		test_ft_insert_node(t_dblist *dlist, int data, t_dblist *to_find);
+
+#endif

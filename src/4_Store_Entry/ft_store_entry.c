26aace521c4c11a2f14e7e0a8dfc684574003f54
Basil ALBADER <baalbade@student.42.fr>
2023-01-24 10:14:32 +0100

    change folder order

---
+++ src/4_Store_Entry/ft_store_entry.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_store_entry.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 08:33:48 by baalbade          #+#    #+#             */
+/*   Updated: 2023/01/17 08:33:51 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+ * 1. Take in entry
+ * 2. Check entry: ft_check_entry.c
+ * 3. Convert entry: ft_convert_entry.c
+ * 4. Store entry in int arr
+ * 5. Look for doubles: ft_look_for_doubles.c
+ * 6. Check size of array elem: ft_check_int_size.c
+ * 7. If no error found ==> return the array for init stack a
+*/
+
+#include "push_swap.h"
+
+int	*ft_store_entry(char ac, char *av[])
+{
+	int	*arr;
+
+}

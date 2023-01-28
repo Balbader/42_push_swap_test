ea6bc0d7869282ab7a574a0d1fa4f65c44fd6806
Balbader <15971200+Balbader@users.noreply.github.com>
2023-01-28 18:20:24 +0100

    update

--- final/src/entry/ft_look_for_doubles.c
+++ final/src/entry/ft_look_for_doubles.c
@@ -12,18 +12,18 @@
 
 #include "push_swap.h"
 
-int	ft_look_for_doubles(int *data_arr, int data_arr_size)
+int	ft_look_for_doubles(int *arr, int arr_size)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	while (i < data_arr_size)
+	while (i < arr_size)
 	{
 		j = i + 1;
-		while (j < data_arr_size)
+		while (j < arr_size)
 		{
-			if (data_arr[i] == data_arr[j])
+			if (arr[i] == arr[j])
 				return (1);
 			++j;
 		}

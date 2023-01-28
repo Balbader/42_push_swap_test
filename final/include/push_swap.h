9428b3258c9a9d1643091a1a98a5645887651115
Balbader <15971200+Balbader@users.noreply.github.com>
2023-01-28 17:22:42 +0100

    update .h

--- final/include/push_swap.h
+++ final/include/push_swap.h
@@ -18,26 +18,13 @@
 # include <stddef.h>
 # include <stdarg.h>
 
+# include "../libft/include/libft.h"
+
 typedef struct s_dblist
 {
 	int				data;
 	struct s_dblist	*next;
 	struct s_dblist	*prev;
 }				t_dblist;
-
-// ft_printf
-int		ft_printf(const char *str, ...);
-void	ft_print_nb(int nb, int *tot_len);
-void	ft_print_hexa(unsigned int nb, char c, int *tot_len);
-void	ft_print_unsigned(unsigned int u, int *tot_len);
-void	ft_print_ptr(size_t ptr, int *tot_len);
-void	ft_print_char(char c, int *tot_len);
-void	ft_print_str(char *str, int *tot_len);
-
-// Utils
-int		ft_atoi(char *str);
-int		ft_strlen(char *str);
-void	*ft_calloc(size_t nmemb, size_t size);
-
 
 #endif

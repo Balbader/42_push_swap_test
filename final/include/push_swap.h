d10ff2cf0a2ddefd433f37ffde77aa50a82cebfe
Balbader <15971200+Balbader@users.noreply.github.com>
2023-01-30 17:00:17 +0100

    .h

--- final/include/push_swap.h
+++ final/include/push_swap.h
@@ -21,40 +21,48 @@
 # define INT_MAX 2147483647
 # define INT_MIN -2147483648
 
-typedef struct s_dlist
+typedef struct s_list
 {
 	int				data;
-	struct s_dblist	*next;
-	struct s_dblist	*prev;
-}				t_dlist;
+	struct s_list	*next;
+}				t_list;
 
 /* Entry Check */
-
-int			ft_check_entry(char *str);
-int			ft_check_int_size(long data);
-int			ft_convert_entry(const char *str);
-int			ft_get_entry(char **av, int elem, int i);
-int			ft_look_for_doubles(int *arr, int arr_size);
-int			*ft_process_entry(int ac, char **av);
-int			*ft_store_entry(int data, int *entries, int pos);
+int		ft_check_entry(char *str);
+int		ft_check_int_size(long data);
+int		ft_convert_entry(const char *str);
+int		ft_get_entry(char **av, int elem, int i);
+int		ft_look_for_doubles(int *arr, int arr_size);
+int		*ft_process_entry(int ac, char **av);
+int		*ft_store_entry(int data, int *entries, int pos);
 
 /* ft_printf */
+void	ft_print_char(char c, int *tot_len);
+void	ft_print_hexa(unsigned int nb, char c, int *tot_len);
+void	ft_print_nb(int nb, int *tot_len);
+void	ft_print_ptr(size_t ptr, int *tot_len);
+void	ft_print_str(char *str, int *tot_len);
+void	ft_print_unsigned(unsigned int u, int *tot_len);
+int		ft_printf(const char *str, ...);
 
-void		ft_print_char(char c, int *tot_len);
-void		ft_print_hexa(unsigned int nb, char c, int *tot_len);
-void		ft_print_nb(int nb, int *tot_len);
-void		ft_print_ptr(size_t ptr, int *tot_len);
-void		ft_print_str(char *str, int *tot_len);
-void		ft_print_unsigned(unsigned int u, int *tot_len);
-int			ft_printf(const char *str, ...);
+/* d_list */
+void	ft_swap_stack(t_list *stack);
+t_list	*ft_get_last_node(t_list *stack);
 
 /* Utils */
+void	*ft_calloc(size_t nmemb, size_t size);
+void	ft_putstr_fd(char *str, int fd);
+void	ft_free_stack(t_list *stack);
+void	ft_print_err(t_list *stack_a, t_list *stack_b);
+int		ft_isdigit(char c);
+int		ft_strlen(const char *str);
 
-void		*ft_calloc(size_t nmemb, size_t size);
-void		ft_putstr_fd(char *str, int fd);
-void		ft_free_stack(t_dlist *stack);
-void		ft_print_err(t_dlist *stack_a, t_dlist *stack_b);
-int			ft_isdigit(char c);
-int			ft_strlen(const char *str);
+/* Operations */
+void	ft_ra(t_list **stack_a);
+void	ft_rb(t_list **stack_b);
+void	ft_rr(t_list **stack_a, t_list **stack_b);
+void	ft_sa(t_list **stack_a);
+void	ft_sb(t_list **stack_b);
+void	ft_ss(t_list **stack_a, t_list **stack_b);
 
 #endif

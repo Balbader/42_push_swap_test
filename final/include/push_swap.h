4c79499d10d67a630293276ec82f5f7d2646bef4
Balbader <15971200+Balbader@users.noreply.github.com>
2023-01-31 17:25:41 +0100

    .h

--- final/include/push_swap.h
+++ final/include/push_swap.h
@@ -17,10 +17,8 @@
 # include <stdlib.h>
 # include <stddef.h>
 # include <stdarg.h>
+# include <limits.h>
 # include <stdio.h>
-
-# define INT_MAX 2147483647
-# define INT_MIN -2147483648
 
 typedef struct s_list
 {
@@ -30,41 +28,43 @@ typedef struct s_list
 }				t_list;
 
 /* Entry Check */
-int		ft_check_entry(char *str);
-int		ft_check_int_size(long data);
-int		ft_convert_entry(const char *str);
-int		ft_get_entry(char *av, int elem);
-int		*ft_store_entry(int data, int *entries, int pos);
-void	ft_look_for_doubles(int *arr, int arr_size);
+int			ft_check_entry(char *str);
+int			ft_get_entry(char *av, long int elem);
+void		ft_look_for_doubles(int *arr, int arr_size);
+long int	ft_convert_entry(const char *av);
 
-/* stack */
-void	ft_push_node(t_list **dest, t_list **src);
-void	ft_swap_stack(t_list *stack);
-void	ft_rotate_stack(t_list **stack);
-void	ft_reverse_rotate_stack(t_list **stack);
-t_list	*ft_get_last_node(t_list *stack);
-t_list	*ft_get_before_last_node(t_list *stack);
-t_list	*ft_init_stack_a(t_list *stack_a, int *entries, int ac);
+/* Sorting */
+t_list		*ft_sort_case_3(int arr[3], t_list *stack_a);
+
+
+/* Stack */
+void		ft_push_node(t_list **dest, t_list **src);
+void		ft_swap_stack(t_list *stack);
+void		ft_rotate_stack(t_list **stack);
+void		ft_reverse_rotate_stack(t_list **stack);
+t_list		*ft_get_last_node(t_list *stack);
+t_list		*ft_get_before_last_node(t_list *stack);
+t_list		*ft_init_stack_a(t_list *stack_a, int *entries, int ac);
 
 /* Utils */
-void	*ft_calloc(size_t nmemb, size_t size);
-void	ft_putstr_fd(char *str, int fd);
-void	ft_free_stack(t_list **stack);
-void	ft_print_err(t_list **stack_a, t_list **stack_b);
-int		ft_isdigit(char c);
-int		ft_strlen(const char *str);
+void		*ft_calloc(size_t nmemb, size_t size);
+void		ft_putstr_fd(char *str, int fd);
+void		ft_free_stack(t_list **stack);
+void		ft_print_err(t_list **stack_a, t_list **stack_b);
+int			ft_isdigit(char c);
+int			ft_strlen(const char *str);
 
 /* Operations */
-void	ft_pa(t_list **stack_b, t_list **stack_a);
-void	ft_pb(t_list **stack_a, t_list **stack_b);
-void	ft_ra(t_list **stack_a);
-void	ft_rb(t_list **stack_b);
-void	ft_rr(t_list **stack_a, t_list **stack_b);
-void	ft_rra(t_list **stack_a);
-void	ft_rrb(t_list **stack_b);
-void	ft_rrr(t_list **stack_a, t_list **stack_b);
-void	ft_sa(t_list **stack_a);
-void	ft_sb(t_list **stack_b);
-void	ft_ss(t_list **stack_a, t_list **stack_b);
+void		ft_pa(t_list **stack_b, t_list **stack_a);
+void		ft_pb(t_list **stack_a, t_list **stack_b);
+void		ft_ra(t_list **stack_a);
+void		ft_rb(t_list **stack_b);
+void		ft_rr(t_list **stack_a, t_list **stack_b);
+void		ft_rra(t_list **stack_a);
+void		ft_rrb(t_list **stack_b);
+void		ft_rrr(t_list **stack_a, t_list **stack_b);
+void		ft_sa(t_list **stack_a);
+void		ft_sb(t_list **stack_b);
+void		ft_ss(t_list **stack_a, t_list **stack_b);
 
 #endif

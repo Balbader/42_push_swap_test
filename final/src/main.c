084d24405000d6da8210e272d2b6d4c4949008c8
Basil ALBADER <baalbade@student.42.fr>
2023-02-08 09:29:45 +0100

    update main.c

--- final/src/main.c
+++ final/src/main.c
@@ -13,71 +13,80 @@
 #include "push_swap.h"
 
 
-static int	ft_check_av(int ac, char **av)
-{
-	int	i;
+// static int	ft_check_av(int ac, char **av)
+// {
+// 	int	i;
 
-	i = 1;
-	while (i < ac)
-	{
-		ft_check_entry(av[i]);
-		++i;
-	}
-	return (0);
-}
+// 	i = 1;
+// 	while (i < ac)
+// 	{
+// 		ft_check_entry(av[i]);
+// 		++i;
+// 	}
+// 	return (0);
+// }
 
-static int	*ft_init_entries_arr(int ac, char **av)
-{
-	int			*arr;
-	int			arr_size;
-	long int	elem;
-	int			i;
-	int			j;
+// static int	*ft_init_entries_arr(int ac, char **av)
+// {
+// 	int			*arr;
+// 	int			arr_size;
+// 	long int	elem;
+// 	int			i;
+// 	int			j;
 
-	arr_size = ac - 1;
-	arr = (int *)malloc(sizeof(int) * arr_size);
-	if (!arr)
-		return (0);
-	elem = 0;
-	j = 1;
-	i = 0;
-	while (i < arr_size)
-	{
-		elem = ft_get_entry(av[j], elem);
-		arr[i] = elem;
-		++i;
-		++j;
-	}
-	ft_look_for_doubles(arr, arr_size);
-	return (arr);
-}
+// 	arr_size = ac - 1;
+// 	arr = (int *)malloc(sizeof(int) * arr_size);
+// 	if (!arr)
+// 		return (0);
+// 	elem = 0;
+// 	j = 1;
+// 	i = 0;
+// 	while (i < arr_size)
+// 	{
+// 		elem = ft_get_entry(av[j], elem);
+// 		arr[i] = elem;
+// 		++i;
+// 		++j;
+// 	}
+// 	ft_look_for_doubles(arr, arr_size);
+// 	return (arr);
+// }
 
-int	main(int ac, char **av)
+// int	main(int ac, char **av)
+int	main(void)
 {
 	t_list	*stack_a;
-	t_list	*stack_b;
-	int		*entries;
+	// t_list	*stack_b;
+	// int		*entries;
+	int		*rand_arr;
+	int		size;
 
-	if (ac < 2)
-		return (0);
+	// if (ac < 2)
+	// 	return (0);
 	stack_a = NULL;
-	stack_b = NULL;
+	// stack_b = NULL;
 
-	ft_check_av(ac, av);
+	// ft_check_av(ac, av);
 
-	entries = ft_init_entries_arr(ac, av);
-	stack_a = ft_init_stack_a(stack_a, entries, ac - 1);
+	// entries = ft_init_entries_arr(ac, av);
+	// stack_a = ft_init_stack_a(stack_a, entries, ac - 1);
+	//
+	size = 20;
+	rand_arr = (int *)malloc(sizeof(int) * size);
+	if (!rand_arr)
+		return (0);
+	stack_a = ft_init_rand_stack(rand_arr, 0, size);
+	ft_print_stack(&stack_a, "rand_stack");
 
 	if (ft_stack_is_sorted(stack_a))
 		return (0);
-	else if (ac - 1 == 2)
-		ft_sa(&stack_a);
-	else if (ac - 1 == 3)
-		stack_a = ft_sort_case_3(entries, stack_a);
-	else if (ac - 1 == 4)
-		stack_a = ft_sort_case_4(stack_a, stack_b);
-	else if (ac - 1 == 5)
-		stack_a = ft_sort_case_5(stack_a, stack_b);
-
+	// else if (ac - 1 == 2)
+	// 	ft_sa(&stack_a);
+	// else if (ac - 1 == 3)
+	// 	stack_a = ft_sort_case_3(rand_arr, stack_a);
+	// else if (ac - 1 == 4)
+	// 	stack_a = ft_sort_case_4(stack_a, stack_b);
+	// else if (ac - 1 == 5)
+	// 	stack_a = ft_sort_case_5(stack_a, stack_b);
 	return (0);
 }

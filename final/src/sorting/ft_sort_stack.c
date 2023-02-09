765c15812da79cd3a6f5c305c11201a29cc8dab7
Basil ALBADER <baalbade@student.42.fr>
2023-02-09 17:34:31 +0100

    add sorting alogrith -- WIP

--- final/src/sorting/ft_sort_stack.c
+++ final/src/sorting/ft_sort_stack.c
@@ -12,5 +12,33 @@
 
 #include "push_swap.h"
 
-t_list	*ft_sort_stack(t_list *stack_a, t_list *stack_b);
+static int	ft_find_pivot(t_list *stack)
+{
+	t_list	*temp;
+	int		stack_size;
+	int		pivot;
 
+	temp = NULL;
+	temp = ft_pre_sort_stack(stack);
+	temp = ft_re_init_index(temp);
+	stack_size = ft_get_stack_size(temp);
+	while (temp)
+		{
+			if (temp->index == stack_size / 2)
+				pivot = temp->data;
+			temp = temp->next;
+		}
+	return (pivot);
+}
+
+void	ft_sort_stack(t_list *stack_a, t_list *stack_b)
+{
+	int	pivot;
+
+	pivot = ft_find_pivot(stack_a);
+	printf("pivot: %d\n\n", pivot);
+	if (stack_a->data > pivot)
+		ft_pb(&stack_a, &stack_b);
+	else
+		ft_ra(&stack_a);
+}

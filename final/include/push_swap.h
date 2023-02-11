8c2cf46ed01554e3b0b9b40c65adf08885b278d4
Basil ALBADER <baalbade@e1r4p1.clusters.42paris.fr>
2023-02-11 16:03:57 +0100

    .h

--- final/include/push_swap.h
+++ final/include/push_swap.h
@@ -84,16 +84,16 @@ void		ft_rotate_stack(t_list **stack);
 void		ft_reverse_rotate_stack(t_list **stack);
 
 /* Operations */
-void		ft_pa(t_list **stack_b, t_list **stack_a);
-void		ft_pb(t_list **stack_a, t_list **stack_b);
-void		ft_ra(t_list **stack_a);
-void		ft_rb(t_list **stack_b);
-void		ft_rr(t_list **stack_a, t_list **stack_b);
-void		ft_rra(t_list **stack_a);
-void		ft_rrb(t_list **stack_b);
-void		ft_rrr(t_list **stack_a, t_list **stack_b);
-void		ft_sa(t_list **stack_a);
-void		ft_sb(t_list **stack_b);
-void		ft_ss(t_list **stack_a, t_list **stack_b);
+void		ft_pa(t_list **stack_b, t_list **stack_a, int hit_count);
+void		ft_pb(t_list **stack_a, t_list **stack_b, int hit_count);
+void		ft_ra(t_list **stack_a, int hit_count);
+void		ft_rb(t_list **stack_b, int hit_count);
+void		ft_rr(t_list **stack_a, t_list **stack_b, int hit_count);
+void		ft_rra(t_list **stack_a, int hit_count);
+void		ft_rrb(t_list **stack_b, int hit_count);
+void		ft_rrr(t_list **stack_a, t_list **stack_b, int hit_count);
+void		ft_sa(t_list **stack_a, int hit_count);
+void		ft_sb(t_list **stack_b, int hit_count);
+void		ft_ss(t_list **stack_a, t_list **stack_b, int hit_count);
 
 #endif
